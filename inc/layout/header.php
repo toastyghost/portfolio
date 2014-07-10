5e7102db95e7a96df3328ab05ee1431660e9e539
Josh Clark <toastyghost@live.com>
2014-07-10 20:12:40 +0100

    corrected assets folder name in header

--- inc/layout/header.php
+++ inc/layout/header.php
@@ -11,23 +11,23 @@
 		<link href="http://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet" type="text/css">
 		<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300" rel="stylesheet" type="text/css">
 		
-		<link href="lib/css/style.css?<?=rand()?>" rel="stylesheet" media="screen" type="text/css"/>
+		<link href="assets/css/style.css?<?=rand()?>" rel="stylesheet" media="screen" type="text/css"/>
 		
-		<link rel="apple-touch-icon" sizes="57x57" href="lib/icons/apple-touch-icon-57x57.png">
-		<link rel="apple-touch-icon" sizes="114x114" href="lib/icons/apple-touch-icon-114x114.png">
-		<link rel="apple-touch-icon" sizes="72x72" href="lib/icons/apple-touch-icon-72x72.png">
-		<link rel="apple-touch-icon" sizes="144x144" href="lib/icons/apple-touch-icon-144x144.png">
-		<link rel="apple-touch-icon" sizes="60x60" href="lib/icons/apple-touch-icon-60x60.png">
-		<link rel="apple-touch-icon" sizes="120x120" href="lib/icons/apple-touch-icon-120x120.png">
-		<link rel="apple-touch-icon" sizes="76x76" href="lib/icons/apple-touch-icon-76x76.png">
-		<link rel="apple-touch-icon" sizes="152x152" href="lib/icons/apple-touch-icon-152x152.png">
-		<link rel="icon" type="image/png" href="lib/icons/favicon-196x196.png" sizes="196x196">
-		<link rel="icon" type="image/png" href="lib/icons/favicon-160x160.png" sizes="160x160">
-		<link rel="icon" type="image/png" href="lib/icons/favicon-96x96.png" sizes="96x96">
-		<link rel="icon" type="image/png" href="lib/icons/favicon-16x16.png" sizes="16x16">
-		<link rel="icon" type="image/png" href="lib/icons/favicon-32x32.png" sizes="32x32">
+		<link rel="apple-touch-icon" sizes="57x57" href="assets/icons/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="assets/icons/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="assets/icons/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="assets/icons/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="assets/icons/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="assets/icons/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="assets/icons/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="assets/icons/apple-touch-icon-152x152.png">
+		<link rel="icon" type="image/png" href="assets/icons/favicon-196x196.png" sizes="196x196">
+		<link rel="icon" type="image/png" href="assets/icons/favicon-160x160.png" sizes="160x160">
+		<link rel="icon" type="image/png" href="assets/icons/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="assets/icons/favicon-16x16.png" sizes="16x16">
+		<link rel="icon" type="image/png" href="assets/icons/favicon-32x32.png" sizes="32x32">
 		<meta name="msapplication-TileColor" content="#da532c">
-		<meta name="msapplication-TileImage" content="lib/icons/mstile-144x144.png">
+		<meta name="msapplication-TileImage" content="assets/icons/mstile-144x144.png">
 		
 		<script src="//code.jquery.com/jquery.min.js"></script>
 		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
