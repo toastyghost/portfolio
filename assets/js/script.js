861d6ee93a2e6c10dfeaf24ea64c03b9fe1de612
Josh Clark <toastyghost@live.com>
2015-01-20 19:26:46 +0000

    added client-side form validation & filter-dodge 'from' line

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,22 +27,62 @@ $(function(){
 	$('#contact-form').on('submit', function(e) {
 		e.preventDefault();
 
-		var $contact_form = $('#contact-form'),
-			$contact_failure_message = $('#contact-failure-message');
-		
-	    $.ajax({
-			url: 'contact.php',
-			type: 'POST',
-			data: $contact_form.serialize(),
-			dataType: 'json',
-			success: function(data, textStatus, jqXHR) {
-				if (data == 1) {
-					$contact_form.add($contact_failure_message).hide();
-					$('#contact-success-message').show();
-				} else {
-					$contact_failure_message.show();
-				}
+		var error_messages = [],
+			form_data = $('#contact-form').serializeArray(),
+			$contact_failure_message = $('#contact-failure-message'),
+			$contact_text = $('#contact-text');
+
+		if (form_data.length > 0) {
+			if (form_data[0].value == '') {
+				error_messages.push('You must enter your name.');
 			}
-		});
+
+			if (form_data[1].value == '' || !isValidEmail(form_data[1].value)) {
+				error_messages.push('You must enter a valid email address.');
+			}
+
+			if (form_data[5].value == '') {
+				error_messages.push('You must enter some details about your project.');
+			}
+		} else {
+			error_messages.push('You must fill out the form before submitting.');
+		}
+		
+		if (error_messages.length === 0) {
+		    $.ajax({
+				url: 'contact.php',
+				type: 'POST',
+				data: form_data,
+				dataType: 'json',
+				success: function(data, textStatus, jqXHR) {
+					if (data == 1) {
+						$contact_form.add($(
+							$contact_text,
+							$contact_failure_message
+						)).fadeOut();
+						$('#contact-success-message').show();
+					} else {
+						error_messages.push('Inquiry could not be submitted. If this problem persists, please <a href="mailto:toastyghost@live.com">email</a> me your project details.');
+					}
+				}
+			});
+		}
+
+		if (error_messages.length > 0) {
+			$contact_failure_message[0].innerHTML = '';
+
+			var key = 0;
+			while (key < error_messages.length) {
+				$contact_failure_message[0].innerHTML += error_messages[key] + '<br>';
+				++key;
+			}
+
+			$contact_failure_message.show();
+		}
 	});
 });
+
+function isValidEmail(emailAddress) {
+	var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+	return pattern.test(emailAddress);
+};
