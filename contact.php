753d0b1201367097fe7344112c3d46f8da3169bb
Josh Clark <toastyghost@live.com>
2015-01-21 00:04:43 +0000

    tweaked contact form email spacing

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
 if (!empty($_POST)) {
 	$to = 'toastyghost@live.com';
 	$subject = "Project inquiry from {$_POST['name']} <{$_POST['email']}>";
-	$body = "Name: {$_POST['name']}\r\nEmail: {$_POST['email']}\r\nCompany: {$_POST['company']}\r\nPhone:{$_POST['phone']}\r\nBudget:{$_POST['budget']}\r\n\r\nProject Description:{$_POST['description']}";
+	$body = "Name: {$_POST['name']}\r\nEmail: {$_POST['email']}\r\nCompany: {$_POST['company']}\r\nPhone: {$_POST['phone']}\r\nBudget: {$_POST['budget']}\r\n\r\nProject Description: {$_POST['description']}";
 	$headers = "From: info@khameleon.org\r\nReply-To:{$_POST['email']}\r\nX-Mailer: PHP/{phpversion()}";
 	
 	if (mail($to, $subject, $body, $headers)) {
